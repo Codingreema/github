def4abddf98bcde2546a686d90ee6bec9102274c
Codingreema <dimaabd383@gmail.com>
2024-04-28 10:19:16 +0300

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Rimah on 19/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
